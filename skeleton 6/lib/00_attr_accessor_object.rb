0414aae08fbf66ee9e89998b5868e2a0b0370b89
App Academy Student <email@email.com>
2022-06-28 16:11:15 -0400

    completion of my_attr_accessor

---
+++ skeleton 6/lib/00_attr_accessor_object.rb
@@ -0,0 +1,12 @@
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+    names.each do |method_name|
+      define_method(method_name) do 
+     instance_variable_get("@#{method_name}")
+      end
+      define_method("#{method_name}=") do |arg|
+     instance_variable_set("@#{method_name}", arg)
+      end
+    end
+  end
+end
